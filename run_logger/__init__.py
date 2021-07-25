ac50f7af7ce7ec3929561bac68ee3fabe6b5b156
Ethan Brooks <ethanabrooks@gmail.com>
2021-07-25 10:55:13 -0400

    remove get_logger

--- run_logger/__init__.py
+++ run_logger/__init__.py
@@ -1,5 +1,3 @@
-from contextlib import contextmanager
-
 from run_logger.haura_logger import HasuraLogger
 from run_logger.jsonlines_logger import JSONLinesLogger
 from run_logger.logger import Logger
@@ -9,20 +7,4 @@ __all__ = [
     "Logger",
     "HasuraLogger",
     "JSONLinesLogger",
-    "get_logger",
 ]
-
-
-_names = dict(
-    hasura=lambda: HasuraLogger(),
-    jsonl=lambda: JSONLinesLogger(),
-)
-
-
-@contextmanager
-def get_logger(logger_type="hasura"):
-    thunk = _names.get(logger_type)
-    if thunk is None:
-        raise RuntimeError("Invalid Config")
-    with thunk() as logger:
-        yield logger
