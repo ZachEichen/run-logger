0c0060c07a6b6609645c5a75da97f3dffbcbef84
Ethan Brooks <ethanabrooks@gmail.com>
2021-07-26 10:45:22 -0400

    fix mispelling of hasura

--- run_logger/__init__.py
+++ run_logger/__init__.py
@@ -1,4 +1,4 @@
-from run_logger.haura_logger import Client, HasuraLogger
+from run_logger.hasura_logger import Client, HasuraLogger
 from run_logger.jsonlines_logger import JSONLinesLogger
 from run_logger.logger import Logger
 from run_logger.params import ParamChoice, SweepMethod
