b586ea76d2bc18d1790ee152540ccd6677a05bd2
Ethan Brooks <ethanabrooks@gmail.com>
2021-10-27 18:16:02 -0400

    add main.py

--- run_logger/__init__.py
+++ run_logger/__init__.py
@@ -2,5 +2,23 @@ from run_logger.hasura_logger import Client, HasuraLogger
 from run_logger.jsonlines_logger import JSONLinesLogger
 from run_logger.logger import Logger
 from run_logger.params import ParamChoice, SweepMethod
+from run_logger.main import (
+    NewParams,
+    get_new_params,
+    get_load_params,
+    update_params,
+    initialize,
+)
 
-__all__ = ["Logger", "HasuraLogger", "JSONLinesLogger", "Client"]
+__all__ = [
+    "Logger",
+    "HasuraLogger",
+    "JSONLinesLogger",
+    "Client",
+    "main",
+    "NewParams",
+    "get_new_params",
+    "get_load_params",
+    "update_params",
+    "initialize",
+]
